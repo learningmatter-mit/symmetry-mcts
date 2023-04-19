82cfd325e1d3af7d63c9df30d875370556bb1f77
Akshay Subramanian <akshay_s@mit.edu>
2023-04-19 17:05:27 -0400

    Create Y6 environment class and replace reward with molecular mass

--- MCTS_y6.py
+++ MCTS_y6.py
@@ -12,20 +12,27 @@ import argparse
 
 from chemprop.predict_one import predict_one
 from torch.utils.tensorboard import SummaryWriter
-from utils import (create_dir, inert_atoms, inert_pair_tuple_char, get_side_chains_end_groups,
-                   find_lowest_inert_atom, get_next_actions_opd, check_terminal, propagate_state)
+from utils import create_dir
+from environment import Y6Environment
 from tree_node import Tree_node
+from rdkit import Chem
+from rdkit.Chem import Descriptors
 
 
-class MCTS():
+def compute_molecular_mass(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    return Descriptors.ExactMolWt(mol)
 
-    def __init__(self, C, decay, side_chains, end_groups, property_target=0.77, property_bound=0.2, restricted = True, exploration='non_exp', num_sims=2500):
+class MCTS:
+
+    def __init__(self, C, decay, side_chains, end_groups, environment, property_target=0.77, property_bound=0.2, restricted = True, exploration='non_exp', num_sims=2500):
         
         self.C = C
         self.decay = decay
         self.side_chains = side_chains
         self.end_groups = end_groups
         self.root = Tree_node([],self.C,None,0)
+        self.environment = environment
         
         self.property_target = property_target
         self.property_bound = property_bound
@@ -50,7 +57,7 @@ class MCTS():
         self.num_sims = num_sims
  
     def traverse(self,node,num, **kwargs):
-        if (len(node.children) == 0) or (check_terminal(node.state)):
+        if (len(node.children) == 0) or (self.environment.check_terminal(node.state)):
             return node
         else:
             max_next = -100000
@@ -67,12 +74,12 @@ class MCTS():
     
     def expand(self,node, **kwargs):
         curr_state = node.state
-        next_actions = get_next_actions_opd(curr_state, self.side_chains, self.end_groups)
+        next_actions = self.environment.get_next_actions_opd(curr_state, self.side_chains, self.end_groups)
  
         next_nodes = []
         for na in next_actions:
-            next_state = propagate_state(curr_state, na)
-            new_node = Tree_node(next_state, self.C, node, check_terminal(next_state))
+            next_state = self.environment.propagate_state(curr_state, na)
+            new_node = Tree_node(next_state, self.C, node, self.environment.check_terminal(next_state))
             next_nodes.append(new_node)
             node.children.append(new_node)
  
@@ -81,11 +88,11 @@ class MCTS():
 
     def roll_out(self,node, **kwargs):
         state = copy.deepcopy(node.state)
-        while not check_terminal(state):
-            next_actions = get_next_actions_opd(state, self.side_chains, self.end_groups)
+        while not self.environment.check_terminal(state):
+            next_actions = self.environment.get_next_actions_opd(state, self.side_chains, self.end_groups)
             move = np.random.randint(0,len(next_actions))
             next_action = next_actions[move]
-            next_state = propagate_state(state, next_action)
+            next_state = self.environment.propagate_state(state, next_action)
             state = next_state
         return state
         
@@ -104,37 +111,45 @@ class MCTS():
         leaf_node = self.traverse(curr_node, num)
         
         ### expansion and not check_terminal(leaf_node.state)
-        if leaf_node.get_n() != 0 and not check_terminal(leaf_node.state):
+        if leaf_node.get_n() != 0 and not self.environment.check_terminal(leaf_node.state):
             leaf_node = self.expand(leaf_node)
 
         ### simulation/roll_out
         final_state = self.roll_out(leaf_node)
-        prop, uncertainty = predict_one('models/weights_lite', [[final_state['smiles']]])
-        prop = prop[0][0]
-        uncertainty = uncertainty[0]
 
-        reward = -1 * prop
-        writer.add_scalar('prop_reward', reward, num)
-        writer.add_scalar('uncertainty', uncertainty, num)
-        if self.exploration == 'UCB_decay':
-            writer.add_scalar('exploration coeff', self.C, num)
+        reward = compute_molecular_mass(final_state['smiles'])
+        print(reward)
+        writer.add_scalar('molecular mass', reward, num)
+
+        # prop, uncertainty = predict_one('models/weights_lite', [[final_state['smiles']]])
+        # prop = prop[0][0]
+        # uncertainty = uncertainty[0]
+
+        # reward = -1 * prop
+        # writer.add_scalar('prop_reward', reward, num)
+        # writer.add_scalar('uncertainty', uncertainty, num)
+        # if self.exploration == 'UCB_decay':
+        #     writer.add_scalar('exploration coeff', self.C, num)
  
         self.backprop(leaf_node,reward)
+        # if final_state['smiles'] not in self.stable_structures_props.keys():
+        #     self.stable_structures_props[final_state['smiles']] = prop
+        #     self.stable_structures_uncs[final_state['smiles']] = uncertainty
+
         if final_state['smiles'] not in self.stable_structures_props.keys():
-            self.stable_structures_props[final_state['smiles']] = prop
-            self.stable_structures_uncs[final_state['smiles']] = uncertainty
- 
+            self.stable_structures_props[final_state['smiles']] = reward
+
         if num % 1 == 0:
             if self.exploration == 'UCB_decay' or self.exploration == 'random':
                 with open('molecules_generated_prop_exploration_{}_num_sims_{}_decay_{}.json'.format(self.exploration, self.num_sims, self.decay), 'w') as f:
                     json.dump(self.stable_structures_props, f)
-                with open('molecules_generated_uncs_exploration_{}_num_sims_{}_decay_{}.json'.format(self.exploration, self.num_sims, self.decay), 'w') as f:
-                    json.dump(self.stable_structures_uncs, f)
+                # with open('molecules_generated_uncs_exploration_{}_num_sims_{}_decay_{}.json'.format(self.exploration, self.num_sims, self.decay), 'w') as f:
+                #     json.dump(self.stable_structures_uncs, f)
             else:
                 with open('molecules_generated_prop_exploration_{}_num_sims_{}_C_{}_decay_{}.json'.format(self.exploration, self.num_sims, self.C, self.decay), 'w') as f:
                     json.dump(self.stable_structures_props, f)
-                with open('molecules_generated_uncs_exploration_{}_num_sims_{}_C_{}_decay_{}.json'.format(self.exploration, self.num_sims, self.C, self.decay), 'w') as f:
-                    json.dump(self.stable_structures_uncs, f)
+                # with open('molecules_generated_uncs_exploration_{}_num_sims_{}_C_{}_decay_{}.json'.format(self.exploration, self.num_sims, self.C, self.decay), 'w') as f:
+                #     json.dump(self.stable_structures_uncs, f)
  
     def run(self, load=False):
         root_state = {
@@ -168,7 +183,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    side_chains, end_groups = get_side_chains_end_groups('fragments/core-fxn-y6-v2.json')
     C = args.C
     decay = args.decay
     exploration = args.exploration
@@ -178,5 +192,7 @@ if __name__ == '__main__':
     create_dir(TB_LOG_PATH)
     writer = SummaryWriter(TB_LOG_PATH)
 
-    new_sim = MCTS(C, decay, side_chains, end_groups, exploration=exploration, num_sims=num_sims)
+    environment = Y6Environment()
+    side_chains, end_groups = environment.get_side_chains_end_groups('fragments/core-fxn-y6-v2.json')
+    new_sim = MCTS(C, decay, side_chains, end_groups, environment=environment, exploration=exploration, num_sims=num_sims)
     new_sim.run()
---
+++ environment.py
@@ -0,0 +1,119 @@
+import json
+import copy
+import re
+
+from molgen import react
+
+class Y6Environment:
+    def __init__(self):
+        self.inert_atoms = ['He', 'Ne', 'Ar', 'Kr', 'Xe', 'Rn']
+        self.inert_pair_tuple_char = {
+            'He': 'a',
+            'Ne': 'b',
+            'Ar': 'c',
+            'Kr': 'd',
+            'Xe': 'e',
+            'Rn': 'f'
+        }
+        self.empty_state = {
+                    'smiles': '',
+                    'label': '',
+                    'group': '',
+                    'blocks': [{'smiles': ''}]
+                    }
+
+    def get_side_chains_end_groups(self, json_path):
+        f = json.load(open(json_path))
+        side_chains = []
+        end_groups = []
+
+        for mol in f['molecules']:
+            if mol['group'] == 'side_chain':
+                side_chains.append(mol)
+            elif mol['group'] == 'end_group':
+                end_groups.append(mol)
+        return side_chains, end_groups
+
+    def find_lowest_inert_atom(self, str):
+        for atom in self.inert_atoms:
+            if '[' + atom + ']' in str:
+                return atom
+
+    def get_string_actions(self, tp):
+        new_actions = []
+        if tp == 'pos0':
+            new_actions = [
+                lambda str : str.replace('<pos0>', 's'),
+                lambda str : str.replace('<pos0>', 'o'),
+                lambda str : str.replace('<pos0>', 'c(C)c(C)'),
+                lambda str : str.replace('<pos0>', 'cc'),
+                lambda str : str.replace('n<pos0>n', 'cccc'),
+                lambda str : str.replace('n<pos0>n', 'c(F)c(F)c(F)c(F)'),
+                lambda str : str.replace('5n<pos0>nc5', '(F)c(F)'),
+                lambda str : str.replace('<pos0>', 'n([Xe])')
+            ]
+        elif tp == 'pos1':
+            new_actions = [
+                lambda str : str.replace('<pos1>', 'n([Ar])'),
+                lambda str : str.replace('<pos1>', 's'),
+                lambda str : str.replace('<pos1>', 'o')
+            ]
+        elif tp == 'pos2':
+            new_actions = [
+                lambda str : str.replace('<pos2>', 'n([Kr])'),
+                lambda str : str.replace('<pos2>', 's'),
+                lambda str : str.replace('<pos2>', 'o')
+            ]
+        elif tp == 'pos3':
+            new_actions = [
+                lambda str : str.replace('<pos3>', 'n([Rn])'),
+                lambda str : str.replace('<pos3>', 's'),
+                lambda str : str.replace('<pos3>', 'o')
+            ]
+        return new_actions
+
+    def get_next_actions_opd(self, state, side_chains, end_groups):
+        if self.check_terminal(state):
+            new_actions = []
+        elif ('pos0' in state['blocks'][0]['smiles']):
+            new_actions = self.get_string_actions('pos0')
+        elif ('pos1' in state['blocks'][0]['smiles']):
+            new_actions = self.get_string_actions('pos1')
+        elif ('pos2' in state['blocks'][0]['smiles']):
+            new_actions = self.get_string_actions('pos2')
+        elif ('pos3' in state['blocks'][0]['smiles']):
+            new_actions = self.get_string_actions('pos3')
+        elif ('[He]' in state['blocks'][0]['smiles']):
+            new_actions = end_groups
+        elif any(inert in state['blocks'][0]['smiles'] for inert in ['[Ne]', '[Ar]', '[Kr]', '[Xe]', '[Rn]']):
+            new_actions = side_chains
+        return new_actions
+
+    def check_terminal(self, state):
+        if 'blocks' in state and len(state['blocks']) == 0:
+            return 1
+        return 0
+
+    def propagate_state(self, state, action):
+        next_state = copy.deepcopy(self.empty_state)
+        if callable(action) and action.__name__ == "<lambda>": # if it is a string manipulation action
+            next_state['blocks'][0]['smiles'] = action(state['blocks'][0]['smiles'])
+
+            cleaned_smiles = next_state['blocks'][0]['smiles']
+            for inert_atom in self.inert_pair_tuple_char.keys():
+                handle_finder = re.compile("\[(" + inert_atom + ")\]")
+                cleaned_smiles = handle_finder.sub('[H]', cleaned_smiles)
+
+            next_state['smiles'] = cleaned_smiles
+        elif type(action) == dict: # if it is a react-sym action
+            lowest_inert_atom = self.find_lowest_inert_atom(state['blocks'][0]['smiles'])
+
+            if lowest_inert_atom != 'He': # it requires a sidechain
+                modified_action = copy.deepcopy(action)
+                modified_action['blocks'][0]['smiles'] = modified_action['blocks'][0]['smiles'].replace('inert', lowest_inert_atom)
+            else:
+                modified_action = action
+
+            pair_tuple = (self.inert_pair_tuple_char[lowest_inert_atom], self.inert_pair_tuple_char[lowest_inert_atom])
+            next_state = react.run('opd', core=state, functional_group=modified_action, reactive_pos=0, pair_tuple=pair_tuple)[0]
+        return next_state
